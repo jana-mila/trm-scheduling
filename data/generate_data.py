4fdeecb38b33b73cfc5ae5f9332e84a8e8e29e10
jana-mila <jana.osea@mila.quebec>
2026-01-16 11:38:15 -0500

    move

--- data/generate_data.py
+++ data/generate_data.py
@@ -15,31 +15,33 @@ from src.utils import solve_job_shop
 # CONFIGURATION
 # =============================
 
+#%%
 @dataclass
 class Config:
-    VERSION: int = 2                # Choices of only 1 or 2, difference is in problem output. See generate_problem for more details
-    PURPOSE: str = "train"          # Choices of 'train', 'val', 'test'
-    N: int = 500                    # Number of job shop problems to generate
+    VERSION: int = 3                # Choices of only 1 or 2, difference is in problem output. See generate_problem for more details
+    PURPOSE: str = "test"          # Choices of 'train', 'val', 'test'
+    N: int = 300                    # Number of job shop problems to generate
     TIMEOUT_SECONDS: int = 60       # Seconds for solver to find solution before moving on to the next
 
     # Dimensional bounds
-    M: int = 20                     # Maximum number of machines
-    J: int = 20                     # Maximum number of jobs per problems
-    T: int = 20                     # Maximum number of taks per job
-    D: int = 10                     # Maximum duration time units
+    M: int = 20                     # Maximum number of machines (5 or 20)
+    J: int = 20                     # Maximum number of jobs per problems (5 or 20)
+    T: int = 20                     # Maximum number of taks per job (5 or 20)
+    D: int = 10                     # Maximum duration time units (5 or 10)
 
-    MIN_J: int = 3                  # Minimums
-    MIN_T: int = 3
-    MIN_M: int = 3
+    MIN_J: int = 2                  # Minimums
+    MIN_T: int = 2
+    MIN_M: int = 2
     MIN_D: int = 1
 
 cfg = Config()
+print(cfg)
 
 
 # =============================
 # DIRECTORY SETUP
 # =============================
-
+#%%
 def setup_directories(cfg: Config) -> tuple[Path, Path, Path]:
     subset = f"v{cfg.VERSION}/{cfg.PURPOSE}/{cfg.N}"
     base = Path.cwd() / "data" / subset
@@ -78,9 +80,9 @@ def generate_problem(cfg: Config):
     durations = torch.randint(cfg.MIN_D, cfg.D + 1, (j, t))
 
     if cfg.VERSION == 1:
-        # Version 1: problem tensor is [machines, durations]
+        # Version 1 or 3: problem tensor is [machines, durations] # doesn't need to have
         problem = torch.stack((machines, durations), dim=2)
-    elif cfg.VERSION == 2:
+    elif cfg.VERSION == 2 or cfg.VERSION == 3:
         # Version 2: problem tensor is [job_index, task_index, machines, durations]
         js = torch.arange(j).view(-1, 1).repeat(1, t)
         ts = torch.arange(t).repeat(j, 1)
@@ -116,15 +118,35 @@ def save_results(i, problem, solution, dims, solution_txt, dirs):
     with open(cases_dir / f"{file}.txt", "w") as f:
         f.write(case_text)
 
+
+def convert_solution(solution):
+    
+    def get_digit(x, i):
+        return int(x[i])
+
+    arr = np.array(solution).astype(str).squeeze()
+    max_len = 3         # TODO change this in case to change
+    padded = np.char.zfill(arr, max_len)
+    out = np.zeros((*padded.shape, max_len), dtype=int)
+
+    for i in range(max_len):
+        out[..., i] = np.vectorize(lambda x: int(x[i]))(padded)
+
+    return out
+
+#%%
 # =============================
 # MAIN LOOP
 # =============================
 
 if __name__ == '__main__':
+#%%
     solutions_dir, problems_dir, cases_dir = setup_directories(cfg)
     print(f"Generating dataset N={cfg.N}")
 
-    for i in tqdm(range(cfg.N)):
+#%%
+    i = 0
+    while i < cfg.N: 
         problem, problem_input, dims = generate_problem(cfg)
 
         # run solver in separate process
@@ -145,4 +167,10 @@ if __name__ == '__main__':
         if status == "error":
             continue
 
+        if cfg.VERSION == 3:
+            solution = convert_solution(solution)
+        i += 1
+        print(f"Generated problem {i}")
+
+#%%
         save_results(i, problem, torch.tensor(solution), dims, solution_txt, (solutions_dir, problems_dir, cases_dir))
